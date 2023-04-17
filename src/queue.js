d5b0a959b01cf50910bc10722e39f0d8622badbc
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-04-17 15:03:22 +0300

    j

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node {
+  constructor(value) { 
+    this.value=value
+    this.next=null
+} 
+}
+
 class Queue {
+  constructor() { 
+    this.head=null,
+    this.length=0;
+} 
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if(this.length===0){
+       this.head = new Node(value)
+    } else {
+let currentNode=this.head
+while(currentNode.next){
+  currentNode=currentNode.next
+}
+currentNode.next = new Node(value)
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   
+    let elementToShow = this.head.value;
+    if (this.length===1){delete this.head} else {
+        this.head =this.head.next;
+    }
+    this.length--
+    return elementToShow
   }
+
 }
+
+const queue = new Queue();
 
 module.exports = {
   Queue
